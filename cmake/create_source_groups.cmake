99d66a240b88e0dc751e01b4432a51c63469062d
Tariq <tariquake@hotmail.com>
2022-02-09 12:07:41 +0100

    Deleted unnecessary cmake function

--- cmake/create_source_groups.cmake
+++
@@ -1,4 +0,0 @@
-function(create_source_groups source_list)
-    message(STATUS "Creating source groups")
-    source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${source_list})
-endfunction(create_source_groups)
