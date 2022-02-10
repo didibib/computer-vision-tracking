331a6a6a6795d8408a9bfb20245e1cb66306e825
Tariq <tariquake@hotmail.com>
2022-02-10 16:41:44 +0100

    Trying webcam

--- 01-camera-geometric-calibration/main.cpp
+++ 01-camera-geometric-calibration/main.cpp
@@ -4,10 +4,25 @@
 
 Checkerboard checkerboard { 6, 9,  2.5f };
 CameraCalibration calibrator;
+cv::VideoCapture cam(0);
+
 
 int main()
 {
-    calibrator.Calibrate(checkerboard, IMAGES_DIR_STR);
+    //calibrator.Calibrate(checkerboard, IMAGES_DIR_STR);
+
+    cv::Mat frame;
+
+    if (!cam.isOpened())
+    {
+        std::cout << "is not opened";
+    }
+
+    while(true)
+    {
+        cam >> frame;
+        cv::imshow("Video", frame);
+    }
 
     return 0;
 }
